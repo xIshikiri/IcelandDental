123bcfc1d3511a47de086d370b771df22ac85adb
edunni <94446799+edunni@users.noreply.github.com>
2021-12-08 23:33:55 +0100

    Styled register page

--- html/register.php
+++ html/register.php
@@ -54,60 +54,133 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="pl-PL">
+<!DOCTYPE html lang="pl-PL">
+<html>
     <head>
-        <meta charset="UTF-8">
-        <title>Zarejestruj się</title>
+        <meta charset="UTF-8"/>
+        <link rel="icon" href="css/logo_square_120px.png" type="image/icon type">
         <link rel="stylesheet" href="css/styles.css">
+        <script src="scripts/login_anim.js"></script>
+    
+        <title>Iceland Dental</title>
+    
     </head>
     <body>
-        <div>
-            <h2>REJESTRACJA</h2>
-            <p>Wypełnij ten formularz, aby się zarejestrować.</p>
-            <?php   
-            if(isset($_POST['submit'])) {
-                echo $error; 
-            }
-            ?>
-            <form action="" method="post">
-                <div>
-                    <label>Imię: </label>
-                    <input type="text" name="name" required>
-                </div>
-                <div>
-                    <label>Nazwisko: </label>
-                    <input type="text" name="surname" required>
-                </div>
-                <div>
-                    <label>Data urodzenia: </label>
-                    <input type="date" name="dob" required>
-                </div>
-                <div>
-                    <label>Adres: </label>
-                    <input type="text" name="adress" required>
-                </div>
-                <div>
-                    <label>Adres email: </label>
-                    <input type="email" name="email" required>
-                </div>
-                <div>
-                    <label>Nr telefonu: </label>
-                    <input type="text" name="phone" required>
-                </div>
-                <div>
-                    <label>Hasło:  </label>
-                    <input type="password" name="password" required>
-                </div>
-                <div>
-                    <label>Potwierdź hasło:  </label>
-                    <input type="password" name="confirmPassword" required>
-                </div>
-                <div>
-                    <input type="submit" name="submit" value="Zarejestruj">
-                </div>
-                <p>Masz już konto? <a href="login.php">Zaloguj się</a>.</p>
-            </form>
+        <div class="header">
+            <span class="hEmail">Email: adels123456@gmail.com</span>
+            <span class="hTel">Nr. tel: 777 649 754</span>
+            <span class="hTel">Otwarte: pon-pt 8-19</span>
         </div>
+
+        <div class="login_bar">
+            <button class="login_button" onclick="show_login_box()">ZALOGUJ
+                <div class="login_dropdown" id="loginDropdown">
+                    <?php 
+                    if(isset($_SESSION['userid'])){
+                        // echo "<a href='logout.php'><input type='button' name='logoutButton' value='Wyloguj się'></a>";
+                    }
+                    if(!isset($_SESSION['userid'])){
+                        echo "<a href='login.php'><input type='button' class='lol' name='logoutButton' value='Zaloguj się'></a>";
+                        echo "<div class='lol'></div>";
+                    }
+                    ?>
+                </div>
+            </button>
+        </div>
+
+        <!-- <div class="box_main">
+            <a href="index.php" class="box_text">Iceland Dental</a>
+            <div class="box_main_content"></div>
+        </div> -->
+
+        <div class="navbar">
+            <a href="index.php" class="home"><div class="home_bttn"></div></a>
+            <a href="updates.php" class="nav_bttn">Aktualności</a>
+            <a href="pricelist.php" class="nav_bttn">Cennik</a>
+            <a href="about.php" class="nav_bttn">O nas!</a>
+            <a href="contact.php" class="nav_bttn">Kontakt</a>
+        </div>
+
+        <div class="main">
+            <div>
+                <p>REJESTRACJA</p>
+                <?php   
+                    if(isset($_POST['submit'])) {
+                       echo $error; 
+                    }
+                ?>
+                <div class="register_wrap">
+                    <div class="register_block">
+                        <form action="" method="post">
+                        <span>Wypełnij ten formularz, aby się zarejestrować.</span>
+                            <div>
+                                <label>Imię: </label>
+                                <input type="text" name="name" required>
+                            </div>
+                            <div>
+                                <label>Nazwisko: </label>
+                                <input type="text" name="surname" required>
+                            </div>
+                            <div>
+                                <label>Data urodzenia: </label>
+                                <input type="date" name="dob" required>
+                            </div>
+                            <div>
+                                <label>Adres: </label>
+                                <input type="text" name="adress" required>
+                            </div>
+                            <div>
+                                <label>Adres email: </label>
+                                <input type="email" name="email" required>
+                            </div>
+                            <div>
+                                <label>Nr telefonu: </label>
+                                <input type="text" name="phone" required>
+                            </div>
+                            <div>
+                                <label>Hasło:  </label>
+                                <input type="password" name="password" required>
+                            </div>
+                            <div>
+                                <label>Potwierdź hasło:  </label>
+                                <input type="password" name="confirmPassword" required>
+                            </div>
+                            <div>
+                                <input type="submit" name="submit" value="Zarejestruj">
+                            </div>
+                            <span>Masz już konto? <a href="login.php">Zaloguj się</a>.</span>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <div class="footer_div">
+                <div class="footer_url_box">
+                    <a href="#" class="footer_url">TOS</a>
+                </div>
+                <div class="footer_url_box">
+                    <a href="#" class="footer_url">Kontakt</a>
+                </div>
+                <div class="footer_url_box">
+                    <a href="#" class="footer_url">Lorem</a>
+                </div>
+            </div>
+            <div class="footer_div">
+                <div class="footer_logo"></div>
+            </div>
+            <div class="footer_div">
+                <div class="footer_url_box">
+                    <a href="#" class="footer_url">Lorem</a>
+                </div>
+                <div class="footer_url_box">
+                    <a href="#" class="footer_url">Lorem</a>
+                </div>
+                <div class="footer_url_box">
+                    <a href="#" class="footer_url">Lorem</a>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
