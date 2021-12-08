4938f74519fd37301d10b193e6066181bb9d9e74
Hubert <hubert.malkowski@gmail.com>
2021-12-08 21:11:27 +0100

    admin page

---
+++ html/admin.php
@@ -0,0 +1,44 @@
+
+<?php
+//session_start();
+?>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="icon" href="css/logo_square_120px.png" type="image/icon type">
+    <link rel="stylesheet" href="css/about.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <title>Iceland Dental</title>
+
+</head>
+<body>
+
+
+<div class="AdminPanel" id="adminPanel">
+
+
+    <div class="AdminPanelContent">
+        Klienci
+        <a href="#">Stwórz</a>
+        <a href="#">Dodaj</a>
+    </div>
+    <div class="AdminPanelContent">
+        Lekarze
+        <a href="#">Stwórz</a>
+        <a href="#">Dodaj</a>
+    </div>
+    <div class="AdminPanelContent">
+        Personel
+        <a href="#">Stwórz</a>
+        <a href="#">Dodaj</a>
+    </div>
+
+
+
+
+</div>
+
+<script src="scripts/vue.js"></script>
+
+
+</body>
