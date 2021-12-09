e558b6205e1e52b1d14de07bf4d5c4b0399124d1
akissssssssss <redecki28@gmail.com>
2021-12-09 12:15:02 +0100

    FCK EDIT

---
+++ html/edit.php
@@ -0,0 +1,49 @@
+<?php
+require_once "config.php";
+session_start();
+
+$post_array=$_SESSION['post array'];
+$email=$post_array['email'];
+
+?>
+
+
+
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <title>Edycja</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+    <h2>Edycja</h2>
+        <div>
+        <?php
+            if ($query = $db -> prepare("SELECT * FROM Client WHERE email = $email ;")){
+            $query -> execute();
+                                    
+            $result = $query->get_result();
+
+            if ($result -> num_rows > 0) {
+                while($row = $result -> fetch_assoc()) {
+                    echo "{$row['Name']}<br>";
+                    echo "{$row['Surname']}<br>";
+                    echo "{$row['DateOfBirth']}<br>";
+                    echo "{$row['Adress']}<br>";
+                    echo "{$row['PhoneNumber']}<br>";
+                    echo "{$row['Email']}<br>";
+                }
+                } else {
+                    echo "0 results";
+                }
+                $query -> close();
+            }
+
+            mysqli_close($db);              
+        ?>
+        </div>
+        <div>
+
+        </div>
+    </body>
+</html>
---
+++ html/edit_client.php
@@ -0,0 +1,58 @@
+<?php
+require_once "config.php";
+session_start();
+
+?>
+
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <title>Dodaj klienta</title>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        <h2>Lista kont</h2>
+        <div> 
+            <form method="post" action="edit.php">
+                <table>
+                    <tr>
+                        <th></th>
+                        <th>Imię</th>
+                        <th>Nazwisko</th>
+                        <th>Data urodzenia</th>
+                        <th>Adres</th>
+                        <th>Telefon</th>
+                        <th>Email</th>
+                    </tr>
+                    <?php
+                        if ($query = $db -> prepare("SELECT * FROM Client ;")){
+                            $query -> execute();
+                                    
+                            $result = $query->get_result();
+
+                            if ($result -> num_rows > 0) {
+                                while($row = $result -> fetch_assoc()) {
+                                        echo "<tr>";
+                                        echo "<td><input type='submit' name='{$row['ID']}' value='Otwórz'></td>";
+                                        echo "<td>{$row['Name']}</td>";
+                                        echo "<td>{$row['Surname']}</td>";
+                                        echo "<td>{$row['DateOfBirth']}</td>";
+                                        echo "<td>{$row['Adress']}</td>";
+                                        echo "<td>{$row['PhoneNumber']}</td>";
+                                        echo "<td name='email'>{$row['Email']}</td>";
+                                        echo "</tr>";
+                                }
+                            } else {
+                                    echo "0 results";
+                            }
+                            $query -> close();
+                        }
+
+                        mysqli_close($db);
+                        $_SESSION['post array']=$_POST;
+                    ?>
+                </table>
+            </form>
+        </div>
+    </body>
+</html>
