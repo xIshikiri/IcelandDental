e1593c74670de7570fd2ea7b461849bca8cd14ff
Hubert <hubert.malkowski@gmail.com>
2021-12-10 10:53:47 +0100

    added dashboard.php;

---
+++ html/dashboard.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <link rel="stylesheet" href="css/dashboard.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+</head>
+<body>
+
+
+<header>
+    <h1>Iceland Dental</h1>
+</header>
+
+
+<section id="mainSection">
+
+<div class="dashboardMenu">
+
+
+
+</div>
+
+<div id="appointmentsContainer" class="appointmentsContainer">
+
+    <appointment-module patient="Adrian Rybicki" date="12.06.2012" notes="cool visit"></appointment-module>
+
+</div>
+
+</section>
+
+
+<script src="scripts/dashboard-vue.js"></script>
+</body>
+</html>
---
+++ html/scripts/dashboard-vue.js
@@ -0,0 +1,43 @@
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    props: {
+        name: String
+    },
+    template: '<button v-on:click="count++"> {{ name }} clicked me {{ count }} times.</button>'
+})
+
+new Vue({ el: '#components-demo' })
+
+
+
+
+Vue.component('appointmentModule', {
+
+    props: {
+        patient: String,
+        date: String,
+        notes: String,
+    },
+
+    template: "    <div class=\"appointment\">\n" +
+        "        <span class=\"appointmentContentPatient\">Patient: {{patient}}  </span>\n" +
+        "        <span class=\"appointmentContentDate\"> {{date}}</span> <br>\n" +
+        "        <span class=\"appointmentContentNotes\">Notes: {{notes}}</span> <br/>\n" +
+        "        <a href='#'><input type=\"button\" class=\"appointmentContentVisitPanel\" value=\"Visit panel\"></a>\n" +
+        "        <a href='#'><input type=\"button\" class=\"appointmentContentVisitPanel\" value=\"Options\"></a>\n" +
+        "\n" +
+        "    </div>"
+
+
+})
+
+
+new Vue({
+    el: '#appointmentsContainer',
+
+
+})
