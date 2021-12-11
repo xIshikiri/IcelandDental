b3fc30a6367750b1a914bf2564a1d3effdaa544e
edunni <94446799+edunni@users.noreply.github.com>
2021-12-11 04:32:36 +0100

    Tests

--- testyMB/testyMB.js
+++ testyMB/testyMB.js
@@ -1,6 +1,41 @@
 
 
 
+function kolor(){
+    document.body.style.backgroundColor = "var(--varColor)";
+    document.title = " _ _ _ 🚓 _ _ _ ";
+}
 
-document.getElementById("blockID").style = "background-color: red";
-  
+function kolor1(){
+    document.body.style = "background-color: var(--varColor1)";
+    document.title = " _ _ 🚓💨 _ _ ";
+}
+
+
+
+function police(){
+
+    var check = document
+    switch (document.getElementsByName("policeSpeed").values){
+        case 200:
+            speed = "500";
+            break;
+    }
+
+    function startColor(){
+        console.log("red");
+        kolor();
+        setTimeout(startColor1, speed);
+    }
+
+    function startColor1(){
+        console.log("blue");
+        kolor1();
+        setTimeout(startColor, speed);
+    }
+
+    startColor();
+}
+
+
+//jebac to
--- testyMB/testyMB.php
+++ testyMB/testyMB.php
@@ -4,18 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Teścik Micheal the niskie czoło </title>
+    <title id="title"> Teścik Micheal the niskie czoło </title>
 
     <link rel="stylesheet" href="testyMB.css">
     <script src="testyMB.js"></script>
 </head>
 <body>
     
-    <div class="blockCLASS" id="blockID"></div>
+    <input type="button" id="inputID" onclick="kolor()" value="RED"></div>
+    <input type="button" id="inputID" onclick="kolor1()" value="BLU"></div>
+    <input type="button" id="inputID" onclick="police()" value="WEEE WOOO WEEE WOO"></div>
 
-    <div id="konami">
-        <p id="text">H</p>
-    </div>
+    <div id="blockID"></div>
+
+    <input type="radio" id="500" name="policeSpeed" value="500">
+    <label for="500"> 500% SEIZURE WARNING </label><br>
+    <input type="radio" id="200" name="policeSpeed" value="200">
+    <label for="200"> 200% </label><br>
+    <input type="radio" id="100" name="policeSpeed" value="100">
+    <label for="100"> 100% </label><br>
+    <input type="radio" id="50" name="policeSpeed" value="50">
+    <label for="50"> 50% </label><br>
 
 </body>
 </html>
